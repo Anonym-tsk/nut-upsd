6a7311bd9448b57a4f9d1c89737b5980924d3799
Nikolay Vasilchuk <anonym.tsk@gmail.com>
2024-10-02 17:40:05 +0300

    fix

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,39 +1,39 @@
-#!/bin/sh -ex
+#!/bin/sh
 
-if [ -z "$API_PASSWORD" ]
-then
-   API_PASSWORD=$(dd if=/dev/urandom bs=18 count=1 2>/dev/null | base64)
+if [ -z "$API_PASSWORD" ]; then
+  printf "No API_PASSWORD found"
+  API_PASSWORD=$(dd if=/dev/urandom bs=18 count=1 2>/dev/null | base64)
 fi
 
-if [ -z "$ADMIN_PASSWORD" ]
-then
-   ADMIN_PASSWORD=$(dd if=/dev/urandom bs=18 count=1 2>/dev/null | base64)
+if [ -z "$ADMIN_PASSWORD" ]; then
+  printf "No ADMIN_PASSWORD found"
+  ADMIN_PASSWORD=$(dd if=/dev/urandom bs=18 count=1 2>/dev/null | base64)
 fi
 
-# Can write or does not exist (support user defined mounted read-only configs)
-if [ -w /etc/nut/ups.conf ] || [ ! -e /etc/nut/ups.conf ]; then
-cat >/etc/nut/ups.conf <<EOF
+if [ ! -e /etc/nut/ups.conf ]; then
+  printf "Default ups.conf created"
+  cat >/etc/nut/ups.conf <<EOF
 [$UPS_NAME]
 	desc = "$UPS_DESC"
 	driver = $UPS_DRIVER
 	port = $UPS_PORT
 EOF
 else
-    printf "Skipped ups.conf config"
+  printf "Skipped ups.conf config"
 fi
 
-# Can write or does not exist (support user defined mounted read-only configs)
-if [ -w /etc/nut/upsd.conf ] || [ ! -e /etc/nut/upsd.conf ]; then
-cat >/etc/nut/upsd.conf <<EOF
+if [ ! -e /etc/nut/upsd.conf ]; then
+  printf "Default upsd.conf created"
+  cat >/etc/nut/upsd.conf <<EOF
 LISTEN ${API_ADDRESS:-0.0.0.0} ${API_PORT:-3493}
 EOF
 else
-    printf "Skipped upsd.conf config"
+  printf "Skipped upsd.conf config"
 fi
 
-# Can write or does not exist (support user defined mounted read-only configs)
-if [ -w /etc/nut/upsd.users ] || [ ! -e /etc/nut/upsd.users ]; then
-cat >/etc/nut/upsd.users <<EOF
+if [ ! -e /etc/nut/upsd.users ]; then
+  printf "Default upsd.users created"
+  cat >/etc/nut/upsd.users <<EOF
 [admin]
 	password = $ADMIN_PASSWORD
 	actions = set
@@ -45,17 +45,17 @@ cat >/etc/nut/upsd.users <<EOF
 	upsmon master
 EOF
 else
-    printf "Skipped upsd.users config"
+  printf "Skipped upsd.users config"
 fi
 
-# Can write or does not exist (support user defined mounted read-only configs)
-if [ -w /etc/nut/upsmon.conf ] || [ ! -e /etc/nut/upsmon.conf ]; then
-cat >/etc/nut/upsmon.conf <<EOF
+if [ ! -e /etc/nut/upsmon.conf ]; then
+  printf "Default upsmon.conf created"
+  cat >/etc/nut/upsmon.conf <<EOF
 MONITOR $UPS_NAME@localhost 1 monitor $API_PASSWORD master
 SHUTDOWNCMD "$SHUTDOWN_CMD"
 EOF
 else
-    printf "Skipped upsmon.conf config"
+  printf "Skipped upsmon.conf config"
 fi
 
 chgrp -R nut /etc/nut /dev/bus/usb
